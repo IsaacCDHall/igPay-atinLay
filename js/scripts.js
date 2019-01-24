945739cff03b8c0d6286658499b7291c3a1dfd1c
Isaac Hall <isaacdehall@gmail.com>
2019-01-24 11:46:30 -0800

    pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,34 @@
 
-// business logic
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+function translate(str) {
 
+    // for words that start with a vowel:
+    if (["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].indexOf(str[0]) > -1) {
+        return str=str+"way";
+    }
+    // for words that start with one or more consonants
+   else {
+   //check for multiple consonants
+       for (var i = 0; i<str.length; i++){
+           if (["a", "e", "i", "o", "u"].indexOf(str[i]) > -1){
+               var firstcons = str.slice(0, i);
+               var middle = str.slice(i, str.length);
+               str = middle+firstcons+"ay";
+               break;}
+            }
+    return str;}
+}
 // user interface logic
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+  $("form#piggy").submit(function(event) {
     event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-
-    $(".year").text(year);
-
-    if (!result) {                 // same as writing if (result === false)
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
-    }
-
+    var translateInput =$("input").val();
+    console.log(translateInput);
     $("#result").show();
+    $(".translate").text(translate(translateInput));
   });
 });
+
+
+// After lunch goals:
+// Split the string in business logic side
+// then run a for each loop around the entire function to run on each item in array
